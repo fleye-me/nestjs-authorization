206ffda6efc470336ab5ee52b997aa3de9cf121b
Manogel <manoelgomes53@gmail.com>
2023-08-27 21:44:05 -0300

    feat: update index.ts file

--- src/common/authorization/index.ts
+++ src/common/authorization/index.ts
@@ -1,5 +1,6 @@
 export { AuthorizationModule } from './authorization.module';
 export { AuthorizationConfig } from './types/authorizationConfig.type';
 export { PermissionDef } from './types/permission.type';
+export { AuthorizationExampleGuard } from './example/authorizationExample.guard';
 export * from './authorization.constants';
 export { AuthorizationModuleOptionsToken } from './authorization.definition';
