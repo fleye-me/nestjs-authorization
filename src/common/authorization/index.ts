705020dacdc4ea04a970d3ac723ba72db92e42aa
Manogel <manoelgomes53@gmail.com>
2023-08-27 21:41:47 -0300

    feat: build process

---
+++ src/common/authorization/index.ts
@@ -0,0 +1,5 @@
+export { AuthorizationModule } from './authorization.module';
+export { AuthorizationConfig } from './types/authorizationConfig.type';
+export { PermissionDef } from './types/permission.type';
+export * from './authorization.constants';
+export { AuthorizationModuleOptionsToken } from './authorization.definition';
